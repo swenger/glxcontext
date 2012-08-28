77196a09f25efb7b8198088eb22fffad914f9962
swenger <spwenger@googlemail.com>
2012-08-28 14:10:50 +0200

    python wrapper

--- glxcontext.cpp
+++ glxcontext.cpp
@@ -1,42 +1,62 @@
 // g++ -o glxtest glxtest.cpp -lX11 -lGL
+// g++ glxcontext.cpp -lX11 -lGL -I/usr/include/python2.7 -lpython2.7 -lboost_python -DPYTHON_WRAPPER -shared -fPIC -o glxcontext.so
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+
+#include <exception>
+#include <string>
+#include <sstream>
+#include <vector>
+
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
 #include <GL/gl.h>
 #include <GL/glx.h>
-#include <iostream>
-#include <string>
-#include <sstream>
+
+#ifdef PYTHON_WRAPPER
+#include <boost/python.hpp>
+#endif
 
 #define GLX_CONTEXT_MAJOR_VERSION_ARB       0x2091
 #define GLX_CONTEXT_MINOR_VERSION_ARB       0x2092
 typedef GLXContext (*glXCreateContextAttribsARBProc)(Display*, GLXFBConfig, GLXContext, Bool, const int*);
 
+std::vector<std::pair<int, int> > get_default_context_attribs() {
+	std::vector<std::pair<int, int> > context_attribs;
+	context_attribs.push_back(std::pair<int, int>(GLX_CONTEXT_MAJOR_VERSION_ARB, 4));
+	context_attribs.push_back(std::pair<int, int>(GLX_CONTEXT_MINOR_VERSION_ARB, 0));
+	context_attribs.push_back(std::pair<int, int>(GLX_CONTEXT_FLAGS_ARB,
+				GLX_CONTEXT_FORWARD_COMPATIBLE_BIT_ARB | GLX_CONTEXT_CORE_PROFILE_BIT_ARB));
+	return context_attribs;
+}
+
+const std::vector<std::pair<int, int> > default_context_attribs = get_default_context_attribs();
+
 class GLXError: virtual public std::exception {
 	public:
-		GLXError(const char *message) throw(): message(message) {}
+		GLXError(const std::string &message="unknown error") throw(): message(message) {}
 		virtual ~GLXError() throw() {}
-		virtual const char *what() const throw() { return message; }
+		virtual const char *what() const throw() { return message.c_str(); }
 	private:
-		const char *message;
+		const std::string message;
 };
 
 class Context {
 	public:
-		Context();
+		Context(const std::string &display_name=":0.0", const std::vector<std::pair<int, int> > &context_attribs=default_context_attribs);
+		Context(const Context &other);
 		~Context();
 		void bind();
 		bool is_direct() const;
 	private:
-		Context(const Context &);
 
 		void error(const char *message);
 		bool has_glx_extension(const char *name);
 
+		int *refcount;
 		int (*old_error_handler)(Display*, XErrorEvent*);
 		Display *display;
 		int glx_major, glx_minor;
@@ -68,7 +88,22 @@ bool Context::has_glx_extension(const char *name) {
 	return false;
 }
 
-Context::Context():
+Context::Context(const Context &other):
+	refcount(other.refcount),
+	display(other.display),
+	glx_major(other.glx_major),
+	glx_minor(other.glx_minor),
+	num_fb_configs(other.num_fb_configs),
+	fb_configs(other.fb_configs),
+	glXCreateContextAttribsARB(other.glXCreateContextAttribsARB),
+	old_context(other.old_context),
+	context(other.context)
+{
+	(*refcount)++;
+}
+
+Context::Context(const std::string &display_name, const std::vector<std::pair<int, int> > &context_attribs):
+	refcount(new int(1)),
 	display(NULL),
 	glx_major(0),
 	glx_minor(0),
@@ -78,12 +113,12 @@ Context::Context():
 	old_context(NULL),
 	context(NULL)
 {
-	int context_attribs[] = {
-		GLX_CONTEXT_MAJOR_VERSION_ARB, 4,
-		GLX_CONTEXT_MINOR_VERSION_ARB, 0,
-		GLX_CONTEXT_FLAGS_ARB, GLX_CONTEXT_FORWARD_COMPATIBLE_BIT_ARB | GLX_CONTEXT_CORE_PROFILE_BIT_ARB,
-		None
-	};
+	int context_attribs_array[2 * context_attribs.size() + 1];
+	for (size_t i = 0; i < context_attribs.size(); ++i) {
+		context_attribs_array[2 * i] = context_attribs[i].first;
+		context_attribs_array[2 * i + 1] = context_attribs[i].second;
+	}
+	context_attribs_array[2 * context_attribs.size()] = None;
 
 	const int attrib_list[] = {
 		None
@@ -91,7 +126,7 @@ Context::Context():
 
 	old_error_handler = XSetErrorHandler(&error_handler);
 
-	if ((display = XOpenDisplay(":0.0")) == NULL)
+	if ((display = XOpenDisplay(display_name.c_str())) == NULL)
 		throw GLXError("could not open X display");
 	if (!glXQueryVersion(display, &glx_major, &glx_minor))
 		throw GLXError("could not get GLX version");
@@ -112,7 +147,7 @@ Context::Context():
 		throw GLXError("could not find GLX_ARB_create_context extension");
 	if (!(glXCreateContextAttribsARB = (glXCreateContextAttribsARBProc)glXGetProcAddressARB((const GLubyte*)"glXCreateContextAttribsARB")))
 		throw GLXError("could not find glXCreateContextAttribsARB");
-	if (!(context = glXCreateContextAttribsARB(display, fb_configs[0], 0, True, context_attribs)))
+	if (!(context = glXCreateContextAttribsARB(display, fb_configs[0], 0, True, context_attribs_array)))
 		throw GLXError("could not create GLX context");
 }
 
@@ -126,19 +161,42 @@ bool Context::is_direct() const {
 }
 
 Context::~Context() {
-	if (display && old_context)
-		glXMakeContextCurrent(display, old_drawable, old_read_drawable, old_context);
-	if (display && context)
-		glXDestroyContext(display, context);
-	if (display)
-		glXDestroyPbuffer(display, pbuffer);
-	if (fb_configs)
-		XFree(fb_configs);
-	if (display)
-		XCloseDisplay(display);
-	XSetErrorHandler(old_error_handler);
+	(*refcount)--;
+	if (!refcount) {
+		if (display && old_context)
+			glXMakeContextCurrent(display, old_drawable, old_read_drawable, old_context);
+		if (display && context)
+			glXDestroyContext(display, context);
+		if (display)
+			glXDestroyPbuffer(display, pbuffer);
+		if (fb_configs)
+			XFree(fb_configs);
+		if (display)
+			XCloseDisplay(display);
+		XSetErrorHandler(old_error_handler);
+		delete refcount;
+	}
 }
 
+#ifdef PYTHON_WRAPPER
+using namespace boost::python;
+
+void translator(GLXError const &e) {
+	PyErr_SetString(PyExc_RuntimeError, e.what()); // TODO Python class
+}
+
+BOOST_PYTHON_MODULE(glxcontext) {
+	register_exception_translator<GLXError>(translator);
+
+	class_<GLXError>("GLXError")
+		.def("__str__", &GLXError::what)
+		;
+
+	class_<Context>("GLXContext")
+		.add_property("is_direct", &Context::is_direct)
+		;
+}
+#else
 int main(int argc, const char *argv[]) {
 	Context context;
 
@@ -148,4 +206,5 @@ int main(int argc, const char *argv[]) {
 
 	return 0;
 }
+#endif
 
